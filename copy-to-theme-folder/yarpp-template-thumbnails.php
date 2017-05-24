a9571aaf37b2055583841606250556b03fc74306
Marc Tönsing <marc.toensing@gmail.com>
2017-05-24 20:46:38 +0200

    Added new yarpp template.

--- copy-to-theme-folder/yarpp-template-thumbnails.php
+++ copy-to-theme-folder/yarpp-template-thumbnails.php
@@ -1,6 +1,6 @@
 <?php
 /*
-YARPP Template: twentyseventeen thumbnails
+YARPP Template: Thumbnails Anor Londo
 Description: Requires a theme with support for post thumbnails
 Author: Marc Tönsing
 */ ?>
@@ -9,13 +9,13 @@ Author: Marc Tönsing
     <div id="jp-relatedposts" class="jp-relatedposts" style="display: block;">
         <h3 class="jp-relatedposts-headline"><em>Ähnliche Beiträge</em></h3>
         <div class="jp-relatedposts-items jp-relatedposts-items-visual jp-relatedposts-grid ">
-
+			<?php $i = 0; ?>
 			<?php while (have_posts()) : the_post(); ?>
 				<?php if (has_post_thumbnail()):?>
 
-                    <div class="jp-relatedposts-post jp-relatedposts-post0 jp-relatedposts-post-thumbs">
+                    <div class="jp-relatedposts-post jp-relatedposts-post<?php echo $i?> jp-relatedposts-post-thumbs">
                         <a class="jp-relatedposts-post-a" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" rel="nofollow" >
-                            <img class="unveil" data-src="<?php the_post_thumbnail_url( 'yarpp' ); ?>" data-src-rtina="<?php the_post_thumbnail_url( 'yarpp-retina' ); ?>">
+                            <img class="unveil" data-src="<?php the_post_thumbnail_url( 'yarpp' ); ?>" data-src-retina="<?php the_post_thumbnail_url( 'yarpp-retina' ); ?>">
                         </a>
                         <h4 class="jp-relatedposts-post-title">
                             <a class="jp-relatedposts-post-a" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
@@ -23,6 +23,7 @@ Author: Marc Tönsing
                         <p class="jp-relatedposts-post-date" style="display: block;"><?php the_date(); ?></p>
                     </div>
 
+					<?php $i++ ?>
 				<?php endif; ?>
 			<?php endwhile; ?>
 
