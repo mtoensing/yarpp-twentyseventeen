c568301b68910a64fc2df533ba83b5613239150d
Marc Tönsing <marc.toensing@gmail.com>
2017-05-27 12:51:39 +0200

    v1.7

--- yarpp-twentyseventeen.php
+++ yarpp-twentyseventeen.php
@@ -4,7 +4,7 @@
   Plugin URI: http://marc.tv/marctv-wordpress-plugins/
   Description: YARPP related list for twentyseventeen with lazy load.
   GitHub Plugin URI: mtoensing/yarpp-twentyseventeen
-  Version: 1.6
+  Version: 1.7
   Author: MarcDK
   Author URI: https://marc.tv
   License: GPL2
