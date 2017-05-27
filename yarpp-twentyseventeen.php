9e4aa9dfd0878153c37aad41531b008477564c26
Marc Tönsing <marc.toensing@gmail.com>
2017-05-27 15:53:59 +0200

    v1.8

--- yarpp-twentyseventeen.php
+++ yarpp-twentyseventeen.php
@@ -4,7 +4,7 @@
   Plugin URI: http://marc.tv/marctv-wordpress-plugins/
   Description: YARPP related list for twentyseventeen with lazy load.
   GitHub Plugin URI: mtoensing/yarpp-twentyseventeen
-  Version: 1.7
+  Version: 1.8
   Author: MarcDK
   Author URI: https://marc.tv
   License: GPL2
@@ -15,15 +15,15 @@ function yarpp_twentyseventeen_scripts() {
 	wp_enqueue_script(
 		'jquery.unveil',
 		WP_PLUGIN_URL . "/yarpp-twentyseventeen/jquery.unveil.js", array( "jquery" ),
-		1.5,
+		1.7,
 		true );
 
 	wp_enqueue_script(
-		'jquery.yarpp-init',WP_PLUGIN_URL . "/yarpp-twentyseventeen/jquery.unveil-init.js", array( "jquery", "jquery.unveil" ),1.0,true );
+		'jquery.yarpp-init',WP_PLUGIN_URL . "/yarpp-twentyseventeen/jquery.unveil-init.js", array( "jquery", "jquery.unveil" ),1.8,true );
 
 
 	wp_enqueue_style(
-		"yarpp-template-thumbnails",WP_PLUGIN_URL . "/yarpp-twentyseventeen/yarpp-template.css",false,1.5 );
+		"yarpp-template-thumbnails",WP_PLUGIN_URL . "/yarpp-twentyseventeen/yarpp-template.css",false,1.8 );
 
 }
 
