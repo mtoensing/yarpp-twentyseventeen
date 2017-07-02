640166e7c5f775f205778b647b8e6606ff9aebb6
Marc Tönsing <marc.toensing@gmail.com>
2017-07-02 17:03:34 +0200

    error Handling

--- jquery.unveil-init.js
+++ jquery.unveil-init.js
@@ -2,7 +2,10 @@
     $(document).ready(function ($) {
        $("#jp-relatedposts .unveil").unveil(500);
        $("#jp-relatedposts .jp-relatedposts-post-title").each(function() {
-           $(this).after('<p class="jp-relatedposts-post-date" style="display: block;">' + $(this).data('date') + '</p>');
+           var date = $(this).data('date');
+           if(date !== null){
+               $(this).after('<p class="jp-relatedposts-post-date" style="display: block;">' + date + '</p>');
+           }
        });
     });
 })(jQuery);
